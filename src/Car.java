65180f5294ef4a5c23cd245172a18c7527f6f706
PawPrintsInTheDark <an.kyt.ot15@gmail.com>
2024-06-28 21:56:29 +0300

    Initial commit

---
+++ src/Car.java
@@ -0,0 +1,63 @@
+import javax.security.sasl.SaslClient;
+import java.util.Scanner;
+
+public class Car {
+    String carBrand;
+    int speed, weight;
+
+    public Car(int weight, String carBrand) {
+        this.weight = weight;
+        this.carBrand = carBrand;
+    }
+
+    public void turnedOn() {
+        System.out.println("Машина " + carBrand + " завелась");
+        Scanner sc = new Scanner(System.in);
+        Engine engine = new Engine();
+        engine.isRunning();
+        System.out.println("Введи тип резины");
+        Wheels wheels = new Wheels("Michelin", sc.next());
+        wheels.isRunning();
+    }
+
+    private class Engine {
+        public void isRunning() {
+            System.out.println("Двигатель работает");
+            class Piston {
+                public void energyTransformation() {
+                    System.out.println("Поршни работают преобразовывая топливо в энергию для колёс");
+                }
+            }
+            Piston piston = new Piston();
+            piston.energyTransformation();
+        }
+    }
+
+    private class Wheels {
+        String wheelBrand, type;
+
+        public Wheels(String wheelBrand, String type) {
+            this.wheelBrand = wheelBrand;
+            this.type = type;
+        }
+
+        public void isRunning() {
+            System.out.println("Колёса марки " + wheelBrand + " вращаются");
+            if (type.equalsIgnoreCase("зимняя")) {
+                class Spikes {
+                    public void theSpikesWorks() {
+                        int reducedSpeed = speed - speed / 5;
+                        System.out.println("Сцепление с дорогой лучше за счёт зимней резины, но скорость уменьшена до" +
+                                " " + reducedSpeed + " km" +
+                                "\nИ вес машины увеличен до " + (weight + 30) + " kg"
+                        );
+                    }
+                }
+                Spikes spikes = new Spikes();
+                spikes.theSpikesWorks();
+            } else {
+                System.out.println("Колёса вращаются со скоростью " + speed + " km");
+            }
+        }
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        Car car = new Car(1300, "infiniti");
+        car.speed = 200;
+        car.turnedOn();
+    }
+}
