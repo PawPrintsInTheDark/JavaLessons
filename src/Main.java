e4990ab65db3ed991b1ba5acea7e3112338fd261
PawPrintsInTheDark <an.kyt.ot15@gmail.com>
2024-06-30 14:28:08 +0300

    add Exeption solution

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,17 @@
-import nestedInner.Car;
+import exception.Exception;
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws java.lang.Exception {
+        Exception.checkingForOdd(8);
+        try {
+            Exception.checkingForOdd(11);
+        }catch (java.lang.Exception e){
+            System.out.println(e);
+        }
 
-        Car car = new Car(1300, "infiniti");
-        car.speed = 200;
-        car.turnedOn();
+        Exception.checkForNumber("Пять лет назад");
+        Exception.checkForNumber("10 лет назад");
+
     }
 }
---
+++ src/exception/Exception.java
@@ -0,0 +1,13 @@
+package exception;
+
+public class Exception {
+
+    public static void checkingForOdd(int i) throws java.lang.Exception {
+        if(i % 2 == 1) throw new java.lang.Exception("Ошибка! Число нечётное");
+        System.out.println("Число "+i+" чётное");
+    }
+    public static void checkForNumber(String s) throws java.lang.Exception {
+        if(s.matches(".*\\d.*")) throw new java.lang.Exception("Ошибка! Строка содержит числа");
+        System.out.println("Строка: \""+s+"\" не содержит числа");
+    }
+}
