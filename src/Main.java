1ac8b42aa9fc807b65c21f5e9c232b6bc6bc2318
PawPrintsInTheDark <an.kyt.ot15@gmail.com>
2024-07-04 02:29:20 +0300

    add Stream API 2 solution

--- src/Main.java
+++ src/Main.java
@@ -1,31 +1,24 @@
-import stream.Person;
-
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 public class Main {
 
     public static void main(String[] args) {
+        List<Integer> numbers = Arrays.asList(1,3,564,76,3,1,9,4,23,67,3,1);
+        Map<Boolean, List<Integer>> num = numbers.stream().collect(Collectors.partitioningBy(e -> e % 2 == 0));
 
-        List<String> list = Arrays.asList("яблоко", "банан", "апельсин", "клубника", "киви", "брокколи", "огурец","ананас","арбуз");
-        int count = (int) list.stream().filter(el -> el.startsWith("а")).count();
-        System.out.println(count);
+        for (Map.Entry<Boolean, List<Integer>> item : num.entrySet()){
+            if(!item.getKey()) System.out.println("Нечётные числа:");
+            if(item.getKey()) System.out.println("Чётные числа:");
+            for (Integer i: item.getValue()){
+                System.out.print(i+" ");
+            }
+            System.out.println();
+        }
 
-        List<Person> personList = new ArrayList<>();
-
-        personList.add(new Person("Анна", 25));
-        personList.add(new Person("Борис", 30));
-        personList.add(new Person("Виктория", 22));
-        personList.add(new Person("Григорий", 28));
-        personList.add(new Person("Дарья", 35));
-        personList.add(new Person("Евгений", 40));
-        personList.add(new Person("Жанна", 27));
-        personList.add(new Person("Захар", 33));
-        personList.add(new Person("Ирина", 29));
-        personList.add(new Person("Кирилл", 31));
-
-        personList.stream()
-                .filter(person -> person.getAge() > 30).forEach(System.out::println);
+        System.out.println("Только уникальные числа:");
+        numbers.stream().distinct().forEachOrdered(e -> System.out.print(e+" "));
     }
 }
