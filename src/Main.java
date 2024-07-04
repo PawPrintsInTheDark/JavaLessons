8a0e6a9e7c2f8ca58f3d6dc02bf2cefeddbaabf5
PawPrintsInTheDark <an.kyt.ot15@gmail.com>
2024-07-04 03:12:23 +0300

    add Debugger solution

--- src/Main.java
+++ src/Main.java
@@ -1,24 +1,20 @@
 import java.util.Arrays;
-import java.util.List;
-import java.util.Map;
-import java.util.stream.Collectors;
+import java.util.OptionalInt;
 
 public class Main {
 
     public static void main(String[] args) {
-        List<Integer> numbers = Arrays.asList(1,3,564,76,3,1,9,4,23,67,3,1);
-        Map<Boolean, List<Integer>> num = numbers.stream().collect(Collectors.partitioningBy(e -> e % 2 == 0));
+        int[] numbers = {2, 4, 6, 8, 10, 12, 14, 16, 18, 20, 22, 24, 26, 28, 30};
+        OptionalInt res = Arrays.stream(numbers).filter(e -> e % 2 == 0).max();
+        System.out.println("Максимальное число из чётных:");
+        System.out.println(res.getAsInt());
 
-        for (Map.Entry<Boolean, List<Integer>> item : num.entrySet()){
-            if(!item.getKey()) System.out.println("Нечётные числа:");
-            if(item.getKey()) System.out.println("Чётные числа:");
-            for (Integer i: item.getValue()){
-                System.out.print(i+" ");
-            }
-            System.out.println();
+        String str = "Привет, Александр, с праздником, с Новым Годом";
+        String result ="";
+        for (int i = 0; i < str.length(); i++) {
+            char c = str.charAt(i);
+            result += Character.isLowerCase(c) ? Character.toUpperCase(c) : Character.toLowerCase(c);
         }
-
-        System.out.println("Только уникальные числа:");
-        numbers.stream().distinct().forEachOrdered(e -> System.out.print(e+" "));
+        System.out.println(result);
     }
 }
